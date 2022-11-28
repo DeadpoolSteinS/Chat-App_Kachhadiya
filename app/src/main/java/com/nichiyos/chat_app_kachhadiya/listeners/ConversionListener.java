d74b5a8e26ed016b8b2671c1ddc2ab0d70da1239
DeadpoolSteinS <novaldisago@gmail.com>
2022-11-28 22:15:35 +0700

    conversation lists and online feature

---
+++ app/src/main/java/com/nichiyos/chat_app_kachhadiya/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.nichiyos.chat_app_kachhadiya.listeners;
+
+import com.nichiyos.chat_app_kachhadiya.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/nichiyos/chat_app_kachhadiya/network/ApiService.java
@@ -0,0 +1,17 @@
+package com.nichiyos.chat_app_kachhadiya.network;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.POST;
+
+public interface ApiService {
+
+    @POST("send")
+    Call<String> sendMessage(
+            @HeaderMap HashMap<String, String> headers,
+            @Body String messageBody
+    );
+}
